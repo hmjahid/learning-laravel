5290a5fe0401eeb1bb7532bf5e208d85c905d866
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2025-04-22 09:16:42 +0600

    Update web.php

--- learning-laravel/routes/web.php
+++ learning-laravel/routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $person = [
+        'name' => 'John Doe', 'age' => 30, 'email' => 'john@example.com'
+    ];
+
+    dump($person);
+    // dd($person); 
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::view( '/about', 'about');
