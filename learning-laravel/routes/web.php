a719aa711d09128dd80d1c476c10bc3455265175
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2025-04-23 08:37:38 +0600

    Learning routing
    Today I have learned more about passing parameters

--- learning-laravel/routes/web.php
+++ learning-laravel/routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
         'name' => 'John Doe', 'age' => 30, 'email' => 'john@example.com'
     ];
 
-    dump($person);
+    // dump($person);
     // dd($person); 
     return view('welcome');
 });
@@ -17,3 +17,16 @@ Route::get('/', function () {
 // });
 
 Route::view( '/about', 'about');
+
+Route::get('/contact/{name}', function ( string $name) {
+    return "My name is =$name";
+});
+
+
+Route::get('/service/{type?}', function(string $type = null) {
+    return "The service is=$type";
+});
+
+Route::get('/course/{id}', function(string $id) {
+    return "The course id is=$id";
+})->whereNumber('id');
